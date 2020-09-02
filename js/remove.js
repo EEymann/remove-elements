efb6bae625602c399194d5767ba2e06a30106404
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-09-02 11:57:57 -0700

    add an example array and practice suggestions for the console.

---
+++ js/remove.js
@@ -0,0 +1,9 @@
+const numbers = [100, 200, 300, 400, 500];
+
+//You can practice using the push and shift methods in the JavaScript console.
+//For example, you can find out how many elements are in an array using the length property, numbers.length, which returns five elements.
+//Add an item to the end, using push. numbers.push(600);
+//To remove an element from the end of the array, type numbers.pop()
+//This returns 600 because the value 600 was the last element in the numbers array.
+//To remove an element from the beginning of an array, type: numbers.shift()
+//Shift also returns the first element, after removing it.
